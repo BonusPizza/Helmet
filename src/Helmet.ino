662ba15a38ea96bcb4d8fa1a1fad1455d915eaa5
BonusPizza <51051786+BonusPizza@users.noreply.github.com>
2024-09-28 22:07:00 +0200

    Add VR logic in loop()

--- src/Helmet.ino
+++ src/Helmet.ino
@@ -6,7 +6,35 @@
 #include <SPI.h>
 #include <VoiceRecognitionV3.h>
 
+// define voice recognition stuff
+VR jarvis(17,16);
+// define Command Constants
+#define wakeword        (0)
+#define visor           (1)
+#define auf             (3)
+#define zu              (4)
+#define light           (2)
+#define an              (5)
+#define aus             (6)
 
+#define toggleSensor    (7)
+#define stop            (8)
+
+uint8_t records[7]; //= {wakeword, visor, auf, zu, light, an, aus, toggleSensor}; 
+uint8_t buf[64];
+
+// voice recognition condition variables
+bool AWAKE = false;
+const long awakeBuffer = 1000;
+unsigned long awakeMillis = 0;
+bool VISOR = false;
+const long visorBuffer = 1000;
+unsigned long visorMillis = 0;
+bool LIGHT = false;
+const long lightBuffer = 1000;
+unsigned long lightMillis = 0;
+
+// define Pin stuff
 #define visorButton 5
 #define eyeButton 15
 
@@ -26,11 +54,14 @@ int rightServoOpen = 180;
 int leftServoClose = 180;
 int rightServoClose = 0; 
 
+// other variables
 int visorSpamm = 0;
 int eyeSpamm = 0;
 
 bool visorOpen = true;
 bool eyesOn = false;
+bool voiceSupport = true;
+bool withSensor = true;
 
 int brightness = 255; // LOW == 0, MAX == 255, HIGH == ?
 
@@ -38,8 +69,6 @@ const long interval = 100;
 unsigned long prevMillis = 0;
 int blinksLeft = 0; // add 1 Blink == blinksleft += 2;
 
-// define voice recognition stuff
-VR jarvis(16,17);
 
 // Initialize Helmet
 void setup() {
@@ -47,9 +76,6 @@ void setup() {
   delay(400);
   
   Serial.begin(115200);
-
-  // initialize Voice Recognition
-  jarvis.begin(9600);
 
   // initialize Pins
   // Button
@@ -62,6 +88,22 @@ void setup() {
   // Sensor
   pinMode(lightSensor, INPUT);
 
+  // initialize Voice Recognition
+  jarvis.begin(9600);
+
+  if(jarvis.clear() == 0){
+    Serial.println("Recognizer cleared.");
+  } else {
+    Serial.println("Connecting to VR3_Module failed. Continuing without Voice Recognition!");
+    voiceSupport = false;
+  }
+  if(voiceSupport){
+    if(jarvis.load((uint8_t)wakeword) >= 0){
+      Serial.println("wakeword loaded");
+    }
+
+    jarvis.load(uint8_t stop);
+  }
   // Helmet boots open so eyelights are off
   shutEyesOff();
   digitalWrite(statusLed, HIGH);
@@ -93,43 +135,122 @@ void loop() {
   int visorBS = digitalRead(visorButton);
   int eyeBS = digitalRead(eyeButton);
 
-  // Check if button is pressed and spamm protection is off
-  // otherwise reduce spamm protection (if on)
-  if(visorSpamm == 0 && visorBS == LOW){
-    // open or close the visor based on its current state
-    if(visorOpen){
-      closeVisor();
-    } else if(!visorOpen){
-      openVisor();
-    }
+  buttonFunctions(visorBS, eyeBS);
 
-    visorSpamm = 10;
-
-  } else {
-    if(visorSpamm > 0){
-      visorSpamm--;
+  if(voiceSupport){
+    int ret = jarvis.recognize(buf, 50);
+    if(ret > 0){
+      switch(buf[1]){
+        case wakeword:
+          AWAKE = true;
+          jarvis.clear();
+          jarvis.load(uint8_t visor);
+          jarvis.load(uint8_t light);
+          jarvis.load(uint8_t toggleSensor);
+          jarvis.load(uint8_t stop);
+          break;
+        case visor:
+          if(AWAKE){
+            VISOR = true;
+            jarvis.clear();
+            jarvis.load(uint8_t auf);
+            jarvis.load(uint8_t zu);
+            jarvis.load(uint8_t stop);
+            AWAKE = false;
+          }
+          break;
+        case auf:
+          if(VISOR){
+            if(!visorOpen){ 
+              openVisor();
+            } else {
+              exceptionHandler(1);
+            }
+            jarvis.clear();
+            jarvis.load(uint8_t wakeword);
+            jarvis.load(uint8_t stop);
+            VISOR = false;
+          }
+          break;
+        case zu:
+          if(VISOR){
+            if(visorOpen){ 
+              closeVisor();
+            } else {
+              exceptionHandler(2);
+            }
+            jarvis.clear();
+            jarvis.load(uint8_t wakeword);
+            jarvis.load(uint8_t stop);
+            VISOR = false;
+          }
+          break;
+        case light:
+          if(AWAKE){
+            LIGHT = true;
+            jarvis.clear();
+            jarvis.load(uint8_t an);
+            jarvis.load(uint8_t aus);
+            jarvis.load(uint8_t stop);
+            AWAKE = false;
+          } 
+          break;
+        case an:
+          if(LIGHT){
+            if(!eyesOn){
+              turnEyesOn();
+            } else {
+              exceptionHandler(3);
+            }
+            jarvis.clear();
+            jarvis.load(uint8_t wakeword);
+            jarvis.load(uint8_t stop);
+            LIGHT = false;
+          }
+          break;
+        case aus:
+          if(LIGHT){
+            if(eyesOn){
+              shutEyesOff();
+            } else {
+              exceptionHandler(4);
+            }
+            jarvis.clear();
+            jarvis.load(uint8_t wakeword);
+            jarvis.load(uint8_t stop);
+            LIGHT = false;
+          }
+          break;
+        case toggleSensor:
+          if(AWAKE){
+            withSensor = !withSensor;
+            jarvis.clear();
+            jarvis.load(uint8_t wakeword);
+            jarvis.load(uint8_t stop);
+            AWAKE = false;
+          }
+          break;
+        case stop:
+          jarvis.clear();
+          jarvis.load(uint8_t wakeword);
+          jarvis.load(uint8_t stop);
+          AWAKE = false;
+          VISOR = false;
+          LIGHT = false;
+          break;
+        default:
+          break;
+      }
     }
   }
 
-  // same thing but for the eyes
-  if(eyeSpamm == 0 && eyeBS == LOW){
-    // Toggle LED Eyes based on their current state
+
+  if(withSensor){
     if(eyesOn){
-      shutEyesOff();
-    } else if(!eyesOn){
-      turnEyesOn();
-    } 
-
-    eyeSpamm = 5;
-
-  } else {
-    if(eyeSpamm > 0){
-      eyeSpamm--;
+      setBrightness(map(analogRead(lightSensor), 0, 4095, 0, 255));
     }
-  }
-
-  if(eyesOn){
-    setBrightness(map(analogRead(lightSensor), 0, 4095, 0, 255));
+  } else {
+    setBrightness(255); //TODO Find standard brightness
   }
 
   if(blinksLeft > 0){
@@ -199,6 +320,44 @@ void setBrightness(int newBrightness){
   }
 }
 
+void buttonFunctions(bool visorBS, bool eyeBS){
+  // Check if button is pressed and spamm protection is off
+  // otherwise reduce spamm protection (if on)
+  if(visorSpamm == 0 && visorBS == LOW){
+    // open or close the visor based on its current state
+    if(visorOpen){
+      closeVisor();
+    } else if(!visorOpen){
+      openVisor();
+    }
+
+    visorSpamm = 10;
+
+  } else {
+    if(visorSpamm > 0){
+      visorSpamm--;
+    }
+  }
+
+  // same thing but for the eyes
+  if(eyeSpamm == 0 && eyeBS == LOW){
+    // Toggle LED Eyes based on their current state
+    if(eyesOn){
+      shutEyesOff();
+    } else if(!eyesOn){
+      turnEyesOn();
+    } 
+
+    eyeSpamm = 5;
+
+  } else {
+    if(eyeSpamm > 0){
+      eyeSpamm--;
+    }
+  }
+}
+
+
 // function to make the user understand what is going wrong
 void exceptionHandler(int errorCode){
   switch (errorCode){
@@ -206,6 +365,15 @@ void exceptionHandler(int errorCode){
     Serial.println("The visor is open!");
     // 2 short blinks on statusLed
     blinksLeft = 4;
+    break;
+  case 2:
+    Serial.println("The visor is closed!");
+    break;
+  case 3:
+    Serial.println("The Eyes are already on!");
+    break;
+  case 4:
+    Serial.println("The Eyes are already off!");
     break;
   default:
     break;
